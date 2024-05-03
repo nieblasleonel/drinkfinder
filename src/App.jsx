ca71b2c892573073938e837aea2268b737098aed
Nieblas <nieblasleonel@gmail.com>
2024-05-03 13:12:05 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center mt-10">
+        <h1 className="text-3xl font-bold">
+          Hello world!
+        </h1>
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
